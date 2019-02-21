d68563cc799dc9302c0c0a0b4f3792134460d9bf
YehorSamoilenko <47282760+YehorSamoilenko@users.noreply.github.com>
2019-02-21 19:35:59 +0200

    SeleniumWD2

---
+++ SeleniumWD2/SeleniumWD2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumWD2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumWD2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26d85eb4-06be-4d49-b9a7-9441569d62e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumWD2/SeleniumWD2/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using Assert = NUnit.Framework.Assert;
+
+namespace SeleniumWD2
+{
+    [TestClass]
+    public class SeleniumWD2
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("https://www.mailinator.com/");
+            driver.FindElement(By.Id("inboxfield")).SendKeys("Yehor_Test@mailinator.com");
+            
+        }
+
+        [Test]
+        public void FindNecessaryEmail()
+        {
+            string from = "Yehor Samoilenko";
+            driver.FindElement(By.XPath("//span[@class='input-group-btn']")).Click();
+            string value = driver.FindElement(By.Id("row_yehor_test-1550747942-931251")).Text;
+
+            if (value.Contains(from))
+            {
+                driver.Navigate().GoToUrl("https://www.google.com/search?q=%D0%A2%D0%B5%D1%81%D1%82+%D0%BF%D1%80%D0%BE%D0%B9%D0%B4%D0%B5%D0%BD&rlz=1C1CHBD_ruUA834UA834&oq=%D0%A2%D0%B5%D1%81%D1%82+%D0%BF%D1%80%D0%BE%D0%B9%D0%B4%D0%B5%D0%BD&aqs=chrome..69i57j0l5.2115j0j7&sourceid=chrome&ie=UTF-8");
+            }
+        }
+        [Test]
+        public void From_and_Subject()
+        {
+            string from = "Yehor Samoilenko";
+            string subject = "Test Mail";
+            driver.FindElement(By.XPath("//span[@class='input-group-btn']")).Click();
+            string value = driver.FindElement(By.Id("row_yehor_test-1550747942-931251")).Text;
+
+            if (value.Contains(from) && value.Contains(subject))
+            {
+                driver.Navigate().GoToUrl("https://www.google.com/search?q=%D0%A2%D0%B5%D1%81%D1%82+%D0%BF%D1%80%D0%BE%D0%B9%D0%B4%D0%B5%D0%BD&rlz=1C1CHBD_ruUA834UA834&oq=%D0%A2%D0%B5%D1%81%D1%82+%D0%BF%D1%80%D0%BE%D0%B9%D0%B4%D0%B5%D0%BD&aqs=chrome..69i57j0l5.2115j0j7&sourceid=chrome&ie=UTF-8");
+            }
+        }
+        [Test]
+        public void CorectlyBody()
+        {
+            string body = "Hello World!";
+            driver.FindElement(By.XPath("//span[@class='input-group-btn']")).Click();
+            driver.FindElement(By.Id("row_yehor_test-1550747942-931251")).Click();
+            string message = driver.FindElement(By.XPath("//div[@dir='ltr']")).Text;
+
+            if (message.Contains(body))
+            {
+                driver.Navigate().GoToUrl("https://www.google.com/search?q=%D0%A2%D0%B5%D1%81%D1%82+%D0%BF%D1%80%D0%BE%D0%B9%D0%B4%D0%B5%D0%BD&rlz=1C1CHBD_ruUA834UA834&oq=%D0%A2%D0%B5%D1%81%D1%82+%D0%BF%D1%80%D0%BE%D0%B9%D0%B4%D0%B5%D0%BD&aqs=chrome..69i57j0l5.2115j0j7&sourceid=chrome&ie=UTF-8");
+            }
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
